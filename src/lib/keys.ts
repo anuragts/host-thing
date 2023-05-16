8a44f9a58d2710b9779b0036b04ecc77e52c9c30
Anurag30112003 <anuragsharma011011@gmail.com>
2023-05-16 14:44:17 +0530

    auth

--- src/lib/keys.ts
+++ src/lib/keys.ts
@@ -5,7 +5,5 @@ export const keys = {
     PROJECT_ID: process.env.PROJECT_ID,
     BUCKET_ID: process.env.BUCKET_ID,
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-    
-    
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,    
 }
